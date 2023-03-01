dce19a77a4453e64320c7dc119eba7a43f532543
jglowak <125830248+jglowak@users.noreply.github.com>
2023-03-01 01:17:09 -0300

    Update saludo.js

--- js/saludo.js
+++ js/saludo.js
@@ -3,13 +3,13 @@ function mostrarMensaje() {
     const fecha = new Date(); 
     const hora = fecha.getHours(); 
 
-    if(hora >= 5 && hora < 12) {
+    if(hora >= 05 && hora < 12) {
         Buenas = "Buenos Días"; 
         
     } else if (hora >= 12 && hora < 20) {
         Buenas = "Buenas Tardes";
         
-    } else if (hora >= 20 && hora < 5) {
+    } else if (hora >= 20 && hora < 05) {
         Buenas = "Buenas Noches";
         
     } 
